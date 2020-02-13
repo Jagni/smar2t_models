2ebd41b991d12bf4357b002386a2d8c03d299539
Jagni <jagnidasa@gmail.com>
2020-02-12 23:18:20 -0300

    WIP - ATL rules and ATLauncher

--- smar2t/src/smar2t/Main.java
+++ smar2t/src/smar2t/Main.java
@@ -2,18 +2,39 @@ package smar2t;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.eclipse.m2m.atl.emftvm.Model;
+
 import smar2t.W3CMapper;
 import w3c_td.core.Thing;
 
 class ModelToJSON {
 	
+	public final static String IN_METAMODEL = "./model/W3CTD/W3CTD.ecore";
+	public final static String IN_METAMODEL_NAME = "W3CTD";
+	public final static String OUT_METAMODEL = "./model/ThingUI/ThingUI.ecore";
+	public final static String OUT_METAMODEL_NAME = "ThingUI";
+		
+	public final static String TRANSFORMATION_DIR = "./ltransformations/";
+	public final static String TRANSFORMATION_MODULE= "W3CTD2ThingUI";
+	
 	public static void main(String[] args) throws IOException {
 		
 		W3CMapper w3cMapper = new W3CMapper();
 		Thing thing1 = w3cMapper.read(new File("src/main/resources/tds/w3c/coap-lamp.json"));
-		File outputFile = new File("src/main/resources/generated/tdrs/w3c/coap-lamp.json");
-		outputFile.createNewFile();
-		w3cMapper.convertToJSON(thing1, outputFile);
+		
+		String outputXMI = "src/main/resources/generated/xmis/w3c/coap-lamp.xmi";
+		String outputJSON = "src/main/resources/generated/tdrs/w3c/coap-lamp.json";
+
+		w3cMapper.convertToJSON(thing1, outputJSON);
+		w3cMapper.convertToXMI(thing1, outputXMI);
+
+		ATLauncher atlauncher = new ATLauncher();
+		atlauncher.registerInputMetamodel(IN_METAMODEL, IN_METAMODEL_NAME);
+		atlauncher.registerOutputMetamodel(OUT_METAMODEL, OUT_METAMODEL_NAME);
+		Model result = atlauncher.w3c2ui(outputXMI, TRANSFORMATION_DIR, TRANSFORMATION_MODULE);
+		
+		System.out.println(result);
 	}
 	
 }
